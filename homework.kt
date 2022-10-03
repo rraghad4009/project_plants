7ed83d051da544b848a0b9c5e1fbc28d1d35e736
rraghad4009 <114874828+rraghad4009@users.noreply.github.com>
2022-10-03 14:17:15 +0300

    Add files via upload

---
+++ homework.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+fun main (){
+    var x: Int = 10
+    var y: Int = 3
+    var m: Int = 6
+    var p: Int = 2
+    val z: Int = 5
+    val r: Int= 5
+    val on :Boolean = true
+    val off: Boolean = false
+    val s = x%y
+    val h =z-y*p
+    val e = y!=p
+    val l = (m+m)/y
+
+    println(" the result : ${off && on}")
+    println("$s")
+    println(" ${z >= r}")
+    println(" the result: ${off || on}")
+    println("$h")
+    println("$e")
+    println("$l")
+
+}
